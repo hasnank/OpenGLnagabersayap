7f7fbfa7b8853b2d61031674dcc9dd6e9c1d91fa
Alfonsus Raditya Arsadjaja <raditya1710@gmail.com>
2017-04-09 12:37:08 +0700

    Meshes

--- OpenGLTutorialProjectWindows/main.cpp
+++ OpenGLTutorialProjectWindows/main.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 #include <GL/glew.h>
 #include "display.h"
+#include "mesh.h"
 #include "shader.h"
 
 int main(int argc, char** argv) {
 	Display display(800, 600, "Hello, World!");
 
+	Vertex vertices[] = { 
+		Vertex(glm::vec3(-0.5f, -0.5f, 0)),
+		Vertex(glm::vec3(0, 0.5f, 0)), 
+		Vertex(glm::vec3(0.5f, -0.5f, 0)) 
+	};
+
+	Mesh mesh(vertices, sizeof(vertices) / sizeof(vertices[0]));
 	Shader shader("./res/basicShader");
+
 	while (!display.IsClosed()) {
 		display.Clear(0.0f, 0.15f, 0.3f, 1.0f);
+	
 		shader.Bind();
+		mesh.Draw();
 
 		display.Update();
 	}
