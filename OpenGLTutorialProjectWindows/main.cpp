90ed71983235aa9d839627e2fb3f20dbd2c20df7
Alfonsus Raditya Arsadjaja <raditya1710@gmail.com>
2017-04-21 03:56:20 +0700

    3D Motion
    Introduction to transformation
    - How to inject it to vertex shader program
    - How to implement the transform
    
    Be careful that Matrix Multiplication is reversed
    Be careful that rotation is not that intuitive for calling it

--- OpenGLTutorialProjectWindows/main.cpp
+++ OpenGLTutorialProjectWindows/main.cpp
@@ -4,6 +4,7 @@
 #include "mesh.h"
 #include "shader.h"
 #include "texture.h"
+#include "transform.h"
 
 int main(int argc, char** argv) {
 	Display display(800, 600, "3D!");
@@ -17,14 +18,27 @@ int main(int argc, char** argv) {
 	Mesh mesh(vertices, sizeof(vertices) / sizeof(vertices[0]));
 	Shader shader("./res/basicShader");
 	Texture texture("./res/bricks.jpg");
+	Transform transform;
+
+	float counter = 0.0f;
 
 	while (!display.IsClosed()) {
 		display.Clear(0.0f, 0.15f, 0.3f, 1.0f);
 
+		float sinCounter = sinf(counter);
+		float cosCounter = cosf(counter);
+
+		transform.GetPos().x = sinf(counter);
+		transform.GetRot().z = counter;
+		transform.SetScale(glm::vec3(cosCounter, cosCounter, cosCounter));
+
 		shader.Bind();
+		texture.Bind(0);
+		shader.Update(transform);
 		mesh.Draw();
 
 		display.Update();
+		counter += 0.001f;
 	}
 	return 0;
 }
--- OpenGLTutorialProjectWindows/shader.h
+++ OpenGLTutorialProjectWindows/shader.h
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <GL/glew.h>
+#include "transform.h"
 
 class Shader
 {
@@ -9,12 +10,19 @@ public:
 	Shader(const std::string& fileName);
 
 	void Bind();
+	void Update(const Transform& transform);
 
 	virtual ~Shader();
 private:
 	static const unsigned int NUM_SHADERS = 2;
 
+	enum {
+		TRANSFORM_U,
+		NUM_UNIFORMS
+	};
+
 	GLuint m_program;
 	GLuint m_shaders[NUM_SHADERS];
+	GLuint m_uniforms[NUM_UNIFORMS];
 };
 
